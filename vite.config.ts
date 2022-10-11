5c564fbb8e692d8f15bc4f9c6badedefe96833c7
李涵祺 <02393963@yto.net.cn>
2022-10-11 17:31:42 +0800

    feat: eslint配置 Signed-off-by: 李涵祺 <02393963@yto.net.cn>

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,54 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import { presetUno, presetAttributify, presetIcons } from "unocss"
-import Unocss from "unocss/vite"
-import {safelist} from './config/unocss'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { presetUno, presetAttributify, presetIcons } from "unocss";
+import Unocss from "unocss/vite";
+import { safelist } from "./config/unocss";
+
 const rollupOptions = {
-  external: ['vue', 'vue-router'],
+  external: ["vue", "vue-router"],
   output: {
     globals: {
       vue: "Vue",
-    }
-  }
-}
+    },
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    Unocss(
-      {
-        safelist,
-        presets: [presetUno(), presetAttributify(), presetIcons()],
-      }
-    )
+    Unocss({
+      safelist,
+      presets: [presetUno(), presetAttributify(), presetIcons()],
+    }),
   ],
   resolve: {
     alias: {
-      'vue': 'vue/dist/vue.esm-bundler.js'
-    }
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
   },
   build: {
     rollupOptions,
-    minify: false,
-    cssCodeSplit: true,
+    minify: "terser", // 启用打包压缩
+    cssCodeSplit: true, // 启用 css 代码分隔
+    sourcemap: true, // 生成 source map 文件
     lib: {
       entry: "./src/entry.ts",
       name: "HqUI",
-      formats: ["es", "umd", "iife","cjs"]
-    }
+      fileName: "hq-ui",
+      formats: ["es", "umd", "iife"]
+    },
   },
-  server:{
-    port:1167
+  server: {
+    port: 1167,
   },
-  test:{
-    globals:true,
-    environment:'happy-dom',
-    transformMode:{
-      web: [/.[tj]sx$/]
-    }
-  }
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    transformMode: {
+      web: [/.[tj]sx$/],
+    },
+  },
 });
